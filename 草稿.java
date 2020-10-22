1e184f1db343358a6fda07362c6b6030f16017a0
Cepper <73273291+Cepper@users.noreply.github.com>
2020-10-22 14:20:14 +0800

    Add files via upload

---
+++ 草稿.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+import java.awt.event.ActionListener;
+
+import java.awt.event.ActionEvent;
+
+public class 草稿 {
+    public static void main(String[] args) { 
+        long startTime=System.nanoTime();
+	    int a=3,b=5,c=9,d;
+	    for(int i = 7;i <=20190324;i++) {
+	        d = (a+b+c)%10000;
+	        a = b;
+	        b = c;
+	        c = d;
+	    }
+	    System.out.println(c);
+	    long endTime=System.nanoTime();
+        System.out.println("当前程序耗时："+(endTime-startTime)+"ns");
+
+	}
+}
